864270ab55e9751e1d5db8990e5e90913f080a6c
jayshilmodi <modija@sheridanc.on.ca>
2022-07-28 17:44:57 -0400

    Home-Page-1

---
+++ HomePageController.java
@@ -0,0 +1,45 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Label contactUs;
+
+    @FXML
+    private Label faq;
+
+    @FXML
+    private Label profile;
+
+    @FXML
+    private Label searchDoctor;
+
+    @FXML
+    private Label searchPharmacy;
+
+    @FXML
+    private Button seeButton;
+
+    @FXML
+    private Label setting;
+
+    @FXML
+    private Label signOut;
+
+    @FXML
+    void addNewBlister(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeBlister(ActionEvent event) {
+
+    }
+
+}
