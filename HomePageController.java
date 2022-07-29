02c3ad94b66cd7fcb40c80a28d232f916113ebc3
jayshilmodi <modija@sheridanc.on.ca>
2022-07-29 15:23:21 -0400

    MultiScreen

--- HomePageController.java
+++ HomePageController.java
@@ -1,11 +1,17 @@
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HomePageController implements Initializable{
 
@@ -41,9 +47,17 @@ public class HomePageController implements Initializable{
 
     }
 
-    @FXML
-    void seeBlister(ActionEvent event) {
+    private Stage  stage;
+    private Scene scene;
+    private Parent root;
 
+    @FXML
+    void seeBlister(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("ListBlister.fxml"));
+        stage = (Stage)((Node) (event.getSource())).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
--- ListBlisterController.java
+++ ListBlisterController.java
@@ -1,15 +1,43 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
-public class ListBlisterController {
+public class ListBlisterController implements Initializable{
 
     @FXML
     private Label homeButton;
 
-    @FXML
-    void goHomePage(MouseEvent event) {
+    private Stage  stage;
+    private Scene scene;
+    private Parent root;
 
+    
+
+    @FXML
+    void goHomePage(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        stage = (Stage)((Node) (event.getSource())).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
